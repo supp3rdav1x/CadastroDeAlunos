5f440c34c3389abe1f4d9969614731a0d7ccabc2
Davi Vitoriano <86158768+supp3rdav1x@users.noreply.github.com>
2022-03-04 07:51:58 -0300

    Add files by upload

---
+++ ProjetoAlunos/Aluno.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAlunos
+{
+    class Aluno
+    {
+        string matricula;
+
+        public string Matricula
+        {
+            get { return matricula; }
+            set { matricula = value; }
+        }
+
+        string nome;
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        DateTime dataNasc;
+
+        public DateTime DataNasc
+        {
+            get { return dataNasc; }
+            set { dataNasc = value; }
+        }
+        string endereco;
+
+        public string Endereco
+        {
+            get { return endereco; }
+            set { endereco = value; }
+        }
+        string cpf;
+
+        public string Cpf
+        {
+            get { return cpf; }
+            set { cpf = value; }
+        }
+        string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        string genero;
+
+        public string Genero
+        {
+            get { return genero; }
+            set { genero = value; }
+        }
+
+
+
+    }
+}
